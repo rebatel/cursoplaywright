9e2cd2e1d600770672876f576fb3882a7ac6edeb
Renato Batel <renato.batel@ibm.com>
2025-01-30 16:54:03 -0300

    aulas 16, 17 e 18

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,11 +1,18 @@
 import { test, expect } from "@playwright/test";
+import { faker } from '@faker-js/faker'
 
-test('deve poder cadastrar uma nova tarefa', async ({ page }) => {
+
+test('deve poder cadastrar uma nova tarefa', async ({ page, request }) => {
+    const taskName = 'Ler um livro de Typescript'
+
+    await request.delete('http://localhost:3333/helper/tasks/' + taskName)
 
     await page.goto('http://localhost:8080')
 
     const inputTaskName = page.locator('input[placeholder="Add a new Task"]')
-    await inputTaskName.fill('Ler um livro de Typescript')
+    await inputTaskName.fill(taskName)
+
+    await page.click('css=button >> text=Create')
 
 
 
